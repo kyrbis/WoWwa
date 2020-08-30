98ebea6bf10657374f352b7149e3acbd86073f33
kyrbis <32635076+kyrbis@users.noreply.github.com>
2020-08-30 04:05:27 +0200

    need manual mana per second input
    mps (mana per second) doesnt work via GetManaRegen(); for mage food :(
    it also has some trouble with a trigger on edge (0s), so needs a bit delay. no good as well.

--- DrinkMe/Actions/OnShow.lua
+++ DrinkMe/Actions/OnShow.lua
@@ -1,7 +1,6 @@
-function get_time_to_mana_percent(percentage)
-	mana_regen_base, casting = GetManaRegen();
-	--print(mana_regen_base);
-	seconds_until_percentage = (UnitPowerMax("player", 0) * percentage - UnitPower("player", 0)) / mana_regen_base;
+function get_time_to_mana_percent(percentage, mps)
+	--mps, casting = GetManaRegen(); --does not work for mage food
+	seconds_until_percentage = (UnitPowerMax("player", 0) * percentage - UnitPower("player", 0)) / mps;
 	if (seconds_until_percentage > 0) then
 		return seconds_until_percentage;
 	else
@@ -15,6 +14,7 @@ end
 
 -- target percentage for mana
 local target_mana_percentage = 0.8;
+local mana_per_second = 83344/20 --equals 4.1k. mana buns do 5% = 5k per second
 
 -- say something
 SendChatMessage("is wolfing down some water.", "EMOTE");
@@ -22,5 +22,5 @@ SendChatMessage("is wolfing down some water.", "EMOTE");
 local current_mana_percentage = get_mana_percentage();
 SendChatMessage("Mana " .. string.format("%.0f", current_mana_percentage * 100) .. "%", "YELL");
 if (target_mana_percentage > current_mana_percentage) then
-	SendChatMessage("Mana " .. string.format("%.0f", target_mana_percentage * 100) .. "% in " .. string.format("%.2f", get_time_to_mana_percent(target_mana_percentage)) .. "s", "YELL");
+	SendChatMessage("Mana " .. string.format("%.0f", target_mana_percentage * 100) .. "% in " .. string.format("%.2f", get_time_to_mana_percent(target_mana_percentage,  mana_per_second)) .. "s", "YELL");
 end
