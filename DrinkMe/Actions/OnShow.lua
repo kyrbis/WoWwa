0a19b9f97317a1e532ce6718a20cf03e11bc1782
kyrbis <32635076+kyrbis@users.noreply.github.com>
2020-12-14 15:06:10 +0100

    timing update

--- DrinkMe/Actions/OnShow.lua
+++ DrinkMe/Actions/OnShow.lua
@@ -2,18 +2,18 @@ function regen_simulation(target_mana_p, current_mana_p)
     local needed_p = target_mana_p - current_mana_p
     local regen_p = 0
     local regen_s = 0
-    while regen_p < needed_p do -- according to https://www.wowhead.com/news=318430/water-regenerates-mana-slower-in-the-first-seconds-of-drinking-in-shadowlands-de#new-base-mana-and-mana-regen-values
-        if (regen_s < 2) then --first two seconds
-            regen_p = regen_p + 2 --regeneration in %
-        elseif (regen_s < 3) then
-            regen_p = regen_p + 3
+    while regen_p <= needed_p do -- according to https://www.wowhead.com/news=318430/water-regenerates-mana-slower-in-the-first-seconds-of-drinking-in-shadowlands-de#new-base-mana-and-mana-regen-values
+        if (regen_s < 3) then --first two seconds
+            regen_p = regen_p + 1 --regeneration in %
         elseif (regen_s < 5) then
-            regen_p = regen_p + 4
-        elseif (regen_s < 6) then
-            regen_p = regen_p + 4
+            regen_p = regen_p + 2
         elseif (regen_s < 7) then
+            regen_p = regen_p + 3
+        elseif (regen_s < 9) then
+            regen_p = regen_p + 4
+        elseif (regen_s < 11) then
             regen_p = regen_p + 5
-        elseif (regen_s > 7) then
+        elseif (regen_s > 11) then
             regen_p = regen_p + 6
         else
             regen_p = regen_p + 5 --fallback default
